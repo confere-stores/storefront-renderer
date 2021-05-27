3f7b221d2451d420dbbc60a42afd6b6fc570a2dc
João Escudero <joaovescudero@gmail.com>
2021-05-27 14:10:19 -0300

    feat: adding new search-engine

--- src/lib/config.js
+++ src/lib/config.js
@@ -8,14 +8,14 @@ module.exports = (settings = null) => {
   const secondaryColor = settings.secondary_color || '#5e1efe'
   const lang = settings.lang || 'en_us'
 // number Store ID from content settings or env var
-  let storeId = settings.store_id || process.env.ECOM_STORE_ID || 1011
+  let storeId = settings.store_id || 1011
   if (typeof storeId === 'string') {
     storeId = parseInt(storeId, 10)
   }
 
 // imported storefront template and components packages
-  const templatePkg = process.env.STOREFRONT_TEMPLATE || '@confere-stores/storefront-template'
-  const componentsPkg = process.env.STOREFRONT_COMPONENTS || '@confere-stores/storefront-components'
+  const templatePkg = '@confere-stores/storefront-template'
+  const componentsPkg = '@confere-stores/storefront-components'
 
   return {
     devMode,
