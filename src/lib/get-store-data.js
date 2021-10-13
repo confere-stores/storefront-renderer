a9812ed0192d6d44becc2347005a661ff0db7e50
Gabriel Mazurco <mazurco066@gmail.com>
2021-10-13 16:45:10 -0300

    feat: added ratings retrieval

--- src/lib/get-store-data.js
+++ src/lib/get-store-data.js
@@ -7,6 +7,11 @@ module.exports = (d, s) => {
   const EcomSearch = require('@ecomplus/search-engine')
   const paths = require('./paths')(d)
   const config = require('./config')(s)
+  const axios = require('axios')
+
+  const reviewApi = axios.create({
+    baseURL: 'http://localhost:3000'  // Change to prod url
+  })
 
   return (storeId = config.storeId, pubSrc = paths.pub, ecomManifest) => {
     return new Promise((resolve, reject) => {
@@ -39,20 +44,30 @@ module.exports = (d, s) => {
             let req
 
             if (client) {
-              // request with ecomClient
-              const { api, endpoint } = client
-              const url = endpoint.replace(':store_id', storeId)
-              req = ecomClient[api]({
-                url,
-                storeId
-              }).then(response => {
-                let body = response.data
-                if (!body._id && body.result) {
-                  // list request
-                  body = body.result
-                }
-                data[prop] = body
-              })
+
+              if (prop !== 'ratings') {
+                // request with ecomClient
+                const { api, endpoint } = client
+                const url = endpoint.replace(':store_id', storeId)
+                req = ecomClient[api]({
+                  url,
+                  storeId
+                }).then(response => {
+                  let body = response.data
+                  if (!body._id && body.result) {
+                    // list request
+                    body = body.result
+                  }
+                  data[prop] = body
+                })
+              } else {
+                const { endpoint } = client
+                req = reviewApi.get(`${endpoint}?storeId=${storeId}`).then(response => {
+                  let body = response.data
+                  data[prop] = body.ponderated
+                })
+              }  
+
             } else if (search) {
               // request with search engine instance
               req = ecomSearch.fetch()
